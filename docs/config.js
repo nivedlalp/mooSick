99c320e7c8e3b189430419b12fda63943b4e0dda
Nived <nived@tutamail.com>
2026-05-21 21:14:30 +0530

    next prev button fix

--- docs/config.js
+++ docs/config.js
@@ -608,12 +608,12 @@ function initPlayerControls() {
         const prevBtn = document.createElement('span');
         prevBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6 6h2v12H6zm3.5 6l8.5 6V6z"/></svg>';
         prevBtn.className = 'aplayer-ext-btn';
-        prevBtn.onclick = () => window.ap1.list.prev();
+        prevBtn.onclick = () => window.ap1.skipBack();
 
         const nextBtn = document.createElement('span');
         nextBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6 18l8.5-6L6 6v12zM16 6v12h2V6h-2z"/></svg>';
         nextBtn.className = 'aplayer-ext-btn';
-        nextBtn.onclick = () => window.ap1.list.next();
+        nextBtn.onclick = () => window.ap1.skipForward();
 
         controls.appendChild(prevBtn);
         controls.appendChild(nextBtn);
